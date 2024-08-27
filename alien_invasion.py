82fd9bd1062fd182b4c5950682f098ea528dbf29
Parthiv Dinesh <parthivdinesh5946@gmail.com>
2024-08-27 21:23:49 +0530

    created a row of ailens

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,9 +35,18 @@ class AlienInvasion:
 
     def _create_fleet(self):
          """create the fleet of aliens."""
-         # Make a alien.
+         # create alien and keep adding aliens untill there is no room left.
+         # spacing between aliens is one alien width
          alien = Alien(self)
-         self.aliens.add(alien)
+         alien_width = alien.rect.width
+
+         current_x = alien_width
+         while current_x <(self.settings.screen_width - 2 * alien_width):
+              new_alien = Alien(self)
+              new_alien.x = current_x
+              new_alien.rect.x = current_x
+              self.aliens.add(new_alien)
+              current_x += 2 * alien_width
 
     def run_game(self):
         """Start the main loop for the game"""
