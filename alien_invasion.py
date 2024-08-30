c8a9df2558938ad0efe8c592df218673ede3e9e4
Parthiv Dinesh <parthivdinesh5946@gmail.com>
2024-08-30 20:46:32 +0530

    droping the fleet and changing the directions

--- alien_invasion.py
+++ alien_invasion.py
@@ -57,6 +57,19 @@ class AlienInvasion:
               new_alien.rect.x = x_position
               new_alien.rect.y = y_position
               self.aliens.add(new_alien)
+
+    def _check_fleet_edges(self):
+         """Respond appropriately if any aliens have reached an edges."""
+         for alien in self.aliens.sprites():
+              if alien.check_edges():
+                   self._change_fleet_direction()
+                   break
+     
+    def _change_fleet_direction(self):
+         """Drop the entire fleet and change the fleet's direction."""
+         for alien in self.aliens.sprites():
+              alien.rect.y += self.settings.fleet_drop_speed
+         self.settings.fleet_direction *= -1
               
 
     def run_game(self):
@@ -81,7 +94,8 @@ class AlienInvasion:
                     self.bullets.remove(bullet)
 
     def _update_aliens(self):
-         """Update the positions of all aliens in the fleet ."""
+         """check if the fleet is at an edge, the update the positions."""
+         self._check_fleet_edges()
          self.aliens.update()
             
 
